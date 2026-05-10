eb8e32982e5a953f12fde221252870adbc3984a6
Anastassiya <anastassiya.ryzhko@gmail.com>
2026-05-10 19:40:10 +0300

    first commit

--- app.py
+++ app.py
@@ -6,9 +6,7 @@ from web.routes import web
 
 
 app = Flask(__name__)
-app.secret_key = "fallback-only-for-dev"
 CORS(app)
-
 app.register_blueprint(api)
 app.register_blueprint(web)
 
@@ -20,6 +18,5 @@ def handle_error(e):
     return jsonify({"error": str(e.description)}), e.code
 
 
-if __name__ == "__main__":
-    port = int(os.environ.get("PORT", 5000))
-    app.run(host="0.0.0.0", port=port)
+if __name__ == '__main__':
+    app.run(debug=True)
